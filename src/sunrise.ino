5e40b39a74be900db73237a487a565c0bda4f159
Rodney Peck <ropeck@google.com>
2017-02-11 22:25:59 -0800

    working with D7 off

--- src/sunrise.ino
+++ src/sunrise.ino
@@ -3,6 +3,16 @@
 #define ON "1"
 #define OFF "0"
 
+#define MUTE_WEMO 1
+
+#define DEBUG 1
+
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)  Serial.println (x)
+#else
+ #define DEBUG_PRINT(x)
+#endif
+
 InternetButton b = InternetButton();
 TCPClient client;
 String swaddr[] = {"10.0.1.10", "10.0.1.18"};
@@ -19,21 +29,26 @@ void setup() {
     b.begin();
     RGB.control(true);
     RGB.brightness(32);  
+    pinMode(D7, INPUT_PULLDOWN);
     Serial.begin(9600);
-  xValue = b.readX();
-  yValue = b.readY();
-  zValue = b.readZ();
+    xValue = b.readX();
+    yValue = b.readY();
+    zValue = b.readZ();
 }
 
 void switchSet(String state, String host) {
   TCPClient client;
   String data1;
+
+  DEBUG_PRINT("switchState "+state+" "+host);
+#ifdef WEMO_MUTE
+  DEBUG_PRINT("muted");
+  return;
+#endif
+
   data1 += "<?xml version=\"1.0\" encoding=\"utf-8\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:SetBinaryState xmlns:u=\"urn:Belkin:service:basicevent:1\"><BinaryState>";
   data1 += state;
   data1 += "%d</BinaryState></u:SetBinaryState></s:Body></s:Envelope>";
-
-  Serial.println("switchState");
-  Serial.println(host);
 
   if (client.connect(host,wemoPort)) {
         client.println("POST /upnp/control/basicevent1 HTTP/1.1");
@@ -65,24 +80,27 @@ void switchOff() {
   }
 }
 
-void loop() {
-    if(!b.allButtonsOff()){
-      switchOff();
+void flashlights() {
       b.rainbow(10);
       delay(100);
       b.allLedsOff();
+}
+
+void loop() {
+// have to ignore button 4 because it's also the D7 led and I want that off
+    
+    if (!(digitalRead(4) && digitalRead(5) && digitalRead(6))) {
+      switchOff();
+      flashlights();
       return;
     }
     int shake = (abs((xValue-b.readX())+abs(yValue-b.readY())+
 		 abs(zValue-b.readZ())));
-//     Serial.println("shake");
-//     Serial.println(shake);
+    DEBUG_PRINT("shake" + shake);
 
     if (shake > 100) {
       switchOn();
-      b.rainbow(10);
-      delay(100);
-      b.allLedsOff();
+      flashlights();
     }
 
     xValue = b.readX();
